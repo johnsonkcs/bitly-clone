1409278a74a454acf059ef6e396946dd0b22e6ef
johnsonkcs <johnsonkcs@gmail.com>
2016-03-25 10:07:27 +0800

    fadein out added

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 	$('#headimg').mouseenter(function() {
        $(this).animate({
-       	height: '+=20%',
+       	height: '+=40%',
        	width: '+=0.05%',
        });
+       $('#midfade').fadeOut('slow');
     });
     $('#headimg').mouseleave(function() {
     	$(this).animate({
-    	height: '-=20%',
+    	height: '-=40%',
     	width: '-=0.05%',
     	});
+    	$('#midfade').fadeIn('slow');
     });
 
      $('.pull-me').click(function() {
